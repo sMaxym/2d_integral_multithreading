8af0fb843bea972c5348737c016bfeaad82d95ac
Titov Ihor <44226375+lurak@users.noreply.github.com>
2020-03-11 14:29:56 +0200

    Update

--- integral_script.py
+++ integral_script.py
@@ -2,6 +2,12 @@ import os
 import sys
 
 
+def take_error(file_name):
+    with open(file_name) as f:
+        lst = f.read().split("\n")
+    return float(lst[0])
+
+
 def file_reader(file_name):
     lst_1 = list()
     lst_2 = list()
@@ -19,11 +25,14 @@ if __name__ == "__main__":
     try:
         n = int(sys.argv[1])
         conf_file = sys.argv[2]
+        ending = str()
         for i in range(n):
-            os.system(os.path.abspath(f"./cmake-build-debug/file_reading.exe {conf_file} >> trash.txt"))
+            if os.name == "nt":
+                ending = '.exe'
+            os.system(os.path.abspath(f"./cmake-build-debug/file_reading{ending} {conf_file} >> trash.txt"))
         res_lst, time_lst = file_reader("out.txt")
         m = min(time_lst)
-        mis = 0.01
+        mis = take_error(conf_file)
         flag = 0
         for j in range(len(res_lst)):
             for k in range(j + 1, len(res_lst)):
